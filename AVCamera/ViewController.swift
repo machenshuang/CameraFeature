78da60b142fe75add37d6b42714d814ef5ff0e76
machenshaung <>
2021-01-10 14:55:55 +0800

    feat：新建工程

---
+++ AVCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVCamera
+//
+//  Created by 马陈爽 on 2021/1/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
